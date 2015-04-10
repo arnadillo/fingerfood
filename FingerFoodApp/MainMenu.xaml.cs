107d5db9621ba8704f710ea7e21c98dbb0c25660
Duoshot <obiwannabe1337@hotmail,.com>
2015-04-09 21:41:34 -0600

    commit

--- FingerFoodApp/MainMenu.xaml.cs
+++ FingerFoodApp/MainMenu.xaml.cs
@@ -21,7 +21,6 @@ namespace FingerFoodApp
     /// </summary>
     public partial class MainMenu : Page
     {
-        CustomerCart total = new CustomerCart(0.00);
         
         //BurgersList burgersList = new BurgersList();
         DrinksList drinksList = new DrinksList();
@@ -29,6 +28,11 @@ namespace FingerFoodApp
         SidesList sidesList = new SidesList();
 
       
+
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
 
         public MainMenu()
         {
@@ -54,6 +58,27 @@ namespace FingerFoodApp
         {
             this.NavigationService.Navigate(drinksList);
         }
+
+        private void burgers_imgPressed(object sender, MouseButtonEventArgs e)
+        {
+            this.NavigationService.Navigate(new BurgersList());
+        }
+
+        private void sides_imgPressed(object sender, MouseButtonEventArgs e)
+        {
+            this.NavigationService.Navigate(new SidesList());
+        }
+
+        private void drinks_imgPressed(object sender, MouseButtonEventArgs e)
+        {
+            this.NavigationService.Navigate(new DrinksList());
+        }
+
+        private void desserts_imgPressed(object sender, MouseButtonEventArgs e)
+        {
+            this.NavigationService.Navigate(new DessertsList());
+        }
+
 
     }
 }
